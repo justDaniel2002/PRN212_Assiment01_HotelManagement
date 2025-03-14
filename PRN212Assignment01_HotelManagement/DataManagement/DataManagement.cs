3183f957c9bbc8378001a04352f47c20f2cb733d
justDaniel2002 <todentsukanai@gmail.com>
2025-03-14 17:12:18 +0700

    login logic

---
+++ PRN212Assignment01_HotelManagement/DataManagement/DataManagement.cs
@@ -0,0 +1,18 @@
+﻿using PRN212Assignment01_HotelManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN212Assignment01_HotelManagement.DataManagement
+{
+    public class DataManagement
+    {
+        public static List<Customer> Customers = new ();
+        public static List<RoomType> RoomTypes = new List<RoomType>();
+        public static List<RoomInformation> RoomInformations = new List<RoomInformation>();
+
+        public static Customer customer;
+    }
+}
--- PRN212Assignment01_HotelManagement/MainWindow.xaml.cs
+++ PRN212Assignment01_HotelManagement/MainWindow.xaml.cs
@@ -20,4 +20,34 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var email = tbEmail.Text;
+        var password = tbPassword.Password;
+
+        if(string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+        {
+            MessageBox.Show("Please enter email and password");
+            return;
+        }
+
+        if(email == "admin" && password == "admin")
+        {
+            MessageBox.Show("Login successfully");
+            /*var adminWindow = new AdminWindow();
+            adminWindow.Show();*/
+            this.Close();
+        }
+        else
+        {
+            var customer = DataManagement.DataManagement.Customers.FirstOrDefault(c => c.EmailAddress.Equals(email) 
+                                                                    && c.Password.Equals(password));
+            if(customer != null)
+            {
+                DataManagement.DataManagement.customer = customer;
+            }
+            else MessageBox.Show("Login failed");
+        }
+    }
 }
